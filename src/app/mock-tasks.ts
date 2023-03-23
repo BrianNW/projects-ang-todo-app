262bb9818fb737aa96cbf5073e520cfbc7a76c85
Brian <briantechdom@gmail.com>
2023-03-23 14:18:00 -0700

    Add fake JSON tasks

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,26 @@
+export const TASKS = [
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'June 1st, at 1:00pm',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Groceries',
+        day: 'June 5th, at 6:00pm',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Turn off oven',
+        day: 'June 1st, at 12:00pm',
+        reminder: true
+    },
+    {
+        id: 4,
+        text: 'Pet dog',
+        day: '',
+        reminder: false
+    }
+]
