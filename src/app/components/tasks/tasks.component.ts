d3052e9db2a9b31b860ce95233b9929bcdf3962d
Brian <briantechdom@gmail.com>
2023-03-31 11:21:45 -0700

    Update on init

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -18,6 +18,7 @@ export class TasksComponent implements OnInit {
   constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.tasks =  this.taskService.getTasks()
   }
 
 }
